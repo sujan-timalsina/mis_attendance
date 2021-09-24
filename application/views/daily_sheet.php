6118d4e241b73aeff96f98d85c77380853b62e28
Sujan Timalsina <sujan.timalsina@apexcollege.edu.np>
2021-09-24 15:22:36 +0545

    Working on Attedance daily sheet

--- application/views/daily_sheet.php
+++ application/views/daily_sheet.php
@@ -14,12 +14,15 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 <body>
     <div class="page-container container bg-light">
-        <form action="">
+        <form action="<?php echo base_url('daily_sheet'); ?>" method="POST">
             <div class="d-flex justify-content-center align-items-center flex-wrap">
                 <div class="form-group col-12 col-md-4 p-1">
                     <label>Employe Name</label>
-                    <select class="form-control">
-                        <option value="NOT NULL">All</option>
+                    <select class="form-control" name="emp_id">
+                        <option value="0">All</option>
+                        <?php foreach ($emp_data as $row) { ?>
+                            <option value="<?php echo $row->employee_id; ?>"><?php echo $row->name; ?></option>
+                        <?php } ?>
                     </select>
                 </div>
                 <div class="form-group col-4 col-md-2 p-1">
@@ -66,7 +69,24 @@ defined('BASEPATH') or exit('No direct script access allowed');
                         </tr>
                     </thead>
                     <tbody>
-
+                        <?php foreach ($emp_details as $row) { ?>
+                            <tr>
+                                <td><?php echo $row->name; ?></td>
+                                <td><?php echo $row->timestamp_date; ?></td>
+                                <td><?php echo $row->timestamp_time; ?></td>
+                                <td>
+                                    <?php if ($row->is_check_in == 0) {
+                                        echo "Check-Out";
+                                    } else {
+                                        echo "Check-In";
+                                    } ?>
+                                </td>
+                                <td><?php echo $row->usr_name; ?></td>
+                                <td><?php echo $row->remarks; ?></td>
+                                <td>[<a>Edit</a>] [<a>Delete</a>]</td>
+                                <td><?php echo $row->check_remarks; ?></td>
+                            </tr>
+                        <?php } ?>
                     </tbody>
                 </table>
             </div>
